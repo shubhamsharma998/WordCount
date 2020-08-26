ff700fe7e93ab3aadf94ee8e3f31ce02f7cb1048
shubham sharma <53936115+shubhamsharma012@users.noreply.github.com>
2020-08-26 12:42:43 +0530

    Add files via upload

---
+++ LineCount.java
@@ -0,0 +1,75 @@
+package Sample;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class LineCount{
+    public static class LineCntMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    Text keyEmit = new Text("Total Lines");
+    private final static IntWritable one = new IntWritable(1);
+
+    public void map(LongWritable key, Text value, Context context){
+        try {
+            context.write(keyEmit, one);
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } 
+        catch (InterruptedException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
+
+    public static class LineCntReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context){
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+        try {
+            context.write(key, new IntWritable(sum));
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } 
+        catch (InterruptedException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
+
+    public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "line count2");
+    job.setJarByClass(LineCount.class);
+    job.setMapperClass(LineCntMapper.class);
+    job.setCombinerClass(LineCntReducer.class);
+    job.setReducerClass(LineCntReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
